267951f2d0f627331d98a889fbe30d2966f3a696
devalvesg <gabrieldamasceno881@outlook.com>
2023-03-02 10:30:55 -0300

    Primeiro desafio

---
+++ src/Desafios/ValorDaVariavel.java
@@ -0,0 +1,17 @@
+package Desafios;
+
+public class ValorDaVariavel {
+	public static void main(String[] args) {
+		int Indice = 13;
+		int Soma = 0;
+		int K = 0;
+		
+		while(K < Indice) {
+			K = K + 1;
+			Soma = Soma + K;
+		}
+		
+		System.out.println(Soma);
+		
+	}
+}
